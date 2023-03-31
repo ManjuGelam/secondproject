e6ff290b59dba2b7277c2267ea79333c448c3c3b
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-31 15:41:16 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using oopsproject;
+namespace secondproject
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Cone cone = new Cone(18.92, 34.12);
+            Console.WriteLine($"Area of Cone is: {cone.GetArea()}\n");
+
+            Circle circ = new Circle(45.36);
+            Console.WriteLine($"Area of Circle is: {circ.GetArea()}\n");
+
+            Triangle trin = new Triangle(34.98, 27.87);
+            Console.WriteLine($"Area of Triangle is: {trin.GetArea()}\n");
+
+            Rectangle rect = new Rectangle(45.29, 76.12);
+            Console.WriteLine($"Area of Rectangle is: {rect.GetArea()}\n");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using oopsproject;
+
+namespace secondproject
+{
+  public  class Class2
+    {
+       public static void Main()
+        {
+            CopyConDemo cd1 = new CopyConDemo(1005);
+            cd1.Display();
+            CopyConDemo cd2 = new CopyConDemo(cd1);
+            cd2.Display();
+            Console.WriteLine();
+           
+            cd1.Display();
+            cd2.Display();
+            Console.WriteLine();
+           
+            cd1.Display();
+            cd2.Display();
+            Console.ReadLine();
+        }
+    }
+}
